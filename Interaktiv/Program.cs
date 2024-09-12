f64e506654544cd592fd2c42cad6b0a70679de31
John Dous <john.dous@elev.ga.ntig.se>
2024-09-12 17:59:16 +0200

    interaktiv

--- Interaktiv/Program.cs
+++ Interaktiv/Program.cs
@@ -1,12 +1,46 @@
-﻿Console.WriteLine("Användarnamn");
-string name = Console.ReadLine();
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Win32.SafeHandles;
 
-if(name == "John")
+Console.WriteLine("Username");
+string name = Console.ReadLine();
+name = name.ToLower();
+
+
 {
-    Console.WriteLine("Välkommen!");
+    Console.WriteLine("WELCOME " + name);
 }
-else
+
+Console.WriteLine(name + ", you are front of a black door, would you lika to open the door or go away. Open ore Away?");
+
+string choice1 = Console.ReadLine().ToLower();
+
+if ( choice1 == "open")
 {
-    Console.WriteLine("Inloggning misslyckats");
+    Console.WriteLine("now, you see a light switch, would you like to turn, On ore off?");
+
+
+    string choice2 = Console.ReadLine().ToLower();
+
+    if (choice2 == "on")
+    {
+        Console.WriteLine("The room is to warm, would you like to jump of the window or be in the warm room, Jump or warm");
+
+        string jump = Console.ReadLine().ToLower();
+        if ( jump == "jump")
+        Console.WriteLine("Congratulations, You are the stupidest man ever, you died");
+    }
+
 }
+
+ if ( choice1 == "away")
+{
+    Console.WriteLine("Go home and play video games alone or play video games with your friends, friends or alone");
+
+    string friends = Console.ReadLine().ToLower();
+ 
+}
+
+
 Console.ReadLine();
+
+
